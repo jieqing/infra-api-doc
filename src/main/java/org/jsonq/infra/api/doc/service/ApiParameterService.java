41fbd7b46725d574b40f233f4b2daaa3ce6ee1ea
jieqin <jieqing@youanmi.com>
2018-08-06 15:30:00 +0800

    子参数

--- src/main/java/org/jsonq/infra/api/doc/service/ApiParameterService.java
+++ src/main/java/org/jsonq/infra/api/doc/service/ApiParameterService.java
@@ -1,8 +1,14 @@
 package org.jsonq.infra.api.doc.service;
 
+import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsonq.common.domain.service.BaseService;
+import org.jsonq.common.repository.po.Entity;
+import org.jsonq.common.repository.query.QueryParam;
+import org.jsonq.common.util.collection.MapUtil;
 import org.jsonq.infra.api.doc.po.ApiParameter;
 import org.jsonq.infra.api.doc.respository.ApiParameterRepository;
 import org.springframework.stereotype.Service;
@@ -19,13 +25,37 @@ public class ApiParameterService extends BaseService<ApiParameter, ApiParameterR
 
     private Logger logger = LogManager.getLogger(this.getClass());
 
-    public Long createAndGetId(ApiParameter apiParameter) {
-        ApiParameter dbApiParameter = repository.get(apiParameter.getUrlId(), apiParameter.getType(), apiParameter.getParentId(), apiParameter.getName());
-        if (dbApiParameter == null) {
-            this.add(apiParameter);
-            return apiParameter.getId();
+    public List<ApiParameter> listByUrlId(Long urlId, Byte type) {
+        List<ApiParameter> apiParameters = repository.listByUrlId(urlId, type);
+
+        Map<Long, ApiParameter> idMap = MapUtil
+                .toMap(apiParameters, Entity.ColumnName.id.getValue());
+        while (true) {
+            boolean hasChild = false;
+            List<ApiParameter> tempList = Lists.newArrayList(apiParameters);
+            for (ApiParameter apiParameter : apiParameters) {
+                if (apiParameter.getParentId() > 0) {
+                    idMap.get(apiParameter.getParentId()).getChildParameters().add(apiParameter);
+                    tempList.remove(apiParameter);
+                    hasChild = true;
+                }
+            }
+            if (!hasChild) {
+                break;
+            }
+            apiParameters = Lists.newArrayList(tempList);
         }
-        return dbApiParameter.getId();
+        return apiParameters;
+    }
+
+    public Long replaceAndGetId(ApiParameter apiParameter) {
+        QueryParam<Enum> checkDbEntityParam = QueryParam.create()
+                .addQuery(ApiParameter.ColumnName.urlId, apiParameter.getUrlId())
+                .addQuery(ApiParameter.ColumnName.type, apiParameter.getType())
+                .addQuery(ApiParameter.ColumnName.parentId, apiParameter.getParentId())
+                .addQuery(ApiParameter.ColumnName.name, apiParameter.getName());
+        repository.replace(apiParameter, checkDbEntityParam);
+        return apiParameter.getId();
     }
 
 
